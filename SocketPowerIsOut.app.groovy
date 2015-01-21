023671c27af6d069cb4910c4d47b9ca9c882fece
twack <twack@wackware.net>
2015-01-21 01:36:21 -0800

    updated SocketPowerIsOut App with metadata

--- SocketPowerIsOut.app.groovy
+++ SocketPowerIsOut.app.groovy
@@ -1,16 +1,29 @@
 /**
+ *  SocketPowerIsOut.groovy
  *
- *  SocketPowerIsOut.app.groovy
+ *  Copyright 2015 Todd Wackford
  *
- *  Alert me of an ac power loss on motion detector by detecting change from
- *  powered to battery. Smartthings hub and internet connection must be working
- *  so this does not work if whole house power is lost. The app works great for
- *  like a GFI or breaker trips in part of the house.
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
  *
- *  Author: todd@wackford.net
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Date: 01-16-2013
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
  */
+ 
+definition(
+    name: "Socket Power Is Out",
+    namespace: "wackware",
+    author: "Todd Wackford",
+    description: "Alert me of an ac power loss on motion detector by detecting change from powered to battery. SmartThings hub and internet connection must be working so this does not work if whole house power is lost. The app works great for like a GFI or breaker trips in part of the house.",
+    category: "Safety & Security",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
 preferences {
 	section("When there's AC power loss on..."){
 		input "myDevice", "capability.battery", title: "Where?"
