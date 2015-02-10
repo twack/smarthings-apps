4b6721a3123e6a31c4603837dd8921f97bda579f
twack <twack@wackware.net>
2015-02-10 07:03:15 -0800

    Update scene-machine.app.groovy

--- scene-machine.app.groovy
+++ scene-machine.app.groovy
@@ -29,7 +29,7 @@
 // Automatically generated. Make future change here.
 definition(
     name: "Scene Machine",
-    namespace: "",
+    namespace: "wackware",
     author: "todd@wackford.net",
     //description: "This app lets the user select from a list of switches or dimmers and record their currents states as a Scene. It is suggested that you name the app during install something like "Scene - Romantic Dinner" or "Scene - Movie Time". Switches can be added, removed or set at new levels by editing and updating the app from the smartphone interface.",
     category: "My Apps",
